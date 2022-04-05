949ab35389aa1dbee8f551fa202a6f47ec7eed31
hao <2524129018@qq.com>
2022-04-05 22:16:20 +0800

    加入图片存在问题

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/header.png'
+console.log(headerBg);
 
 export const Home = () => {
   return (
     <div>
-      你好
+      <div className="x" style={{backgroundImage: 'url(${headerBg})'}}>你好</div>
     </div>
   );
 };
